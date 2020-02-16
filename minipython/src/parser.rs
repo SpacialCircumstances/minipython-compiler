ad219a2bfccb05f27aa0335adfecbf49a420289b
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2020-02-16 15:42:17 +0100

    Create better test

--- minipython/src/parser.rs
+++ minipython/src/parser.rs
@@ -3,20 +3,26 @@ use crate::name::*;
 
 lalrpop_mod!(pub minipython);
 
-fn parse(code: &str) -> Result<Vec<Ast>, String> {
+fn parse(code: &str) -> (NameStore, Result<Vec<Ast>, String>) {
     let mut name_store = NameStore::new();
     let parser = minipython::ProgramParser::new();
-    parser.parse(&mut name_store, code).map_err(|e| format!("{}", e))
+    let res = parser.parse(&mut name_store, code).map_err(|e| format!("{}", e));
+    (name_store, res)
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use crate::ast::Ast::Incr;
 
     #[test]
     fn test_incr_decr() {
         let code = "a+=1";
-        let res = parse(code);
-        assert!(res.is_ok())
+        let (store, res) = parse(code);
+        let expected = vec![Incr {
+            var_name: store.by_index(0).unwrap()
+        }];
+        assert!(res.is_ok());
+        assert_eq!(expected, res.unwrap());
     }
 }
