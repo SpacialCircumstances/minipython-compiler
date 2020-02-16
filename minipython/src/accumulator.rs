dd1e74a4ae0367cad6af79f1f66064153c436c58
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2020-02-16 23:19:16 +0100

    Accumulator can continue after the iterators end to finalize operations

--- minipython/src/accumulator.rs
+++ minipython/src/accumulator.rs
@@ -4,13 +4,13 @@ pub enum Acc<T> {
     Continue
 }
 
-pub struct Accumulator<I, F, S, B> where I: Iterator, F: FnMut(I::Item, &mut S) -> (S, Acc<B>) {
+pub struct Accumulator<I, F, S, B> where I: Iterator, F: FnMut(Option<I::Item>, &mut S) -> (S, Acc<B>) {
     iter: I,
     f: F,
     state: S
 }
 
-impl<I, F, S, B> Accumulator<I, F, S, B> where I: Iterator, F: FnMut(I::Item, &mut S) -> (S, Acc<B>) {
+impl<I, F, S, B> Accumulator<I, F, S, B> where I: Iterator, F: FnMut(Option<I::Item>, &mut S) -> (S, Acc<B>) {
     fn new(iter: I, f: F, state: S) -> Self {
         Accumulator {
             iter,
@@ -20,31 +20,26 @@ impl<I, F, S, B> Accumulator<I, F, S, B> where I: Iterator, F: FnMut(I::Item, &m
     }
 }
 
-impl<I, F, S, B> Iterator for Accumulator<I, F, S, B> where I: Iterator, F: FnMut(I::Item, &mut S) -> (S, Acc<B>) {
+impl<I, F, S, B> Iterator for Accumulator<I, F, S, B> where I: Iterator, F: FnMut(Option<I::Item>, &mut S) -> (S, Acc<B>) {
     type Item = B;
 
     fn next(&mut self) -> Option<Self::Item> {
         loop {
-            match self.iter.next() {
-                None => break None,
-                Some(v) => {
-                    let (new_state, res) = (self.f)(v, &mut self.state);
-                    self.state = new_state;
-                    match res {
-                        Acc::End => break None,
-                        Acc::Next(val) => break Some(val),
-                        Acc::Continue => ()
-                    }
-                }
+            let (new_state, res) = (self.f)(self.iter.next(), &mut self.state);
+            self.state = new_state;
+            match res {
+                Acc::End => break None,
+                Acc::Next(val) => break Some(val),
+                Acc::Continue => ()
             }
         }
     }
 }
 
-pub trait Accumulateable<F, S, B>: Iterator + Sized where F: FnMut(Self::Item, &mut S) -> (S, Acc<B>) {
+pub trait Accumulateable<F, S, B>: Iterator + Sized where F: FnMut(Option<Self::Item>, &mut S) -> (S, Acc<B>) {
     fn accumulate(&mut self, f: F, state: S) -> Accumulator<&mut Self, F, S, B> {
         Accumulator::new(self, f, state)
     }
 }
 
-impl<I, F, S, B> Accumulateable<F, S, B> for I where I: Iterator, F: FnMut(Self::Item, &mut S) -> (S, Acc<B>) {}
+impl<I, F, S, B> Accumulateable<F, S, B> for I where I: Iterator, F: FnMut(Option<Self::Item>, &mut S) -> (S, Acc<B>) {}
