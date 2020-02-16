4d8c1bfbdfa96ee751d6e094b477ddb7e048ea59
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2020-02-16 15:26:46 +0100

    Parse programs & first test

--- minipython/src/parser.rs
+++ minipython/src/parser.rs
@@ -3,8 +3,20 @@ use crate::name::*;
 
 lalrpop_mod!(pub minipython);
 
-fn parse(code: &str) -> Ast {
+fn parse(code: &str) -> Result<Vec<Ast>, String> {
     let mut name_store = NameStore::new();
-    let parser = minipython::ExprParser::new();
-    parser.parse(&mut name_store, code).unwrap()
+    let parser = minipython::ProgramParser::new();
+    parser.parse(&mut name_store, code).map_err(|e| format!("{}", e))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_incr_decr() {
+        let code = "a+=1";
+        let res = parse(code);
+        assert!(res.is_ok())
+    }
 }
