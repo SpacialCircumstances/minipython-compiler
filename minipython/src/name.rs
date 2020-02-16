d125ad63f9e8d68281632691db571c06e50d767d
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2020-02-16 15:40:23 +0100

    Add some helper methods for name store

--- minipython/src/name.rs
+++ minipython/src/name.rs
@@ -24,14 +24,26 @@ impl NameStore {
         }
     }
 
+    pub fn get_by_interned(&self, name: &str) -> Option<Name> {
+        self.store.iter().enumerate().find(|(k, v)| v == &name).map(|(id, _)| Name(id))
+    }
+
     pub fn get(&self, n: Name) -> Option<&String> {
         let id = n.0;
         self.store.get(id)
     }
 
+    pub fn by_index(&self, idx: usize) -> Option<Name> {
+        if idx >= 0 && idx < self.store.len() {
+            Some(Name(idx))
+        } else {
+            None
+        }
+    }
+
     pub fn register(&mut self, name: &str) -> Name {
-        match self.store.iter().enumerate().find(|(k, v)| v == &name) {
-            Some((k, _)) => Name(k),
+        match self.get_by_interned(name) {
+            Some(n) => n,
             None => {
                 let id = self.last_id + 1;
                 self.last_id = id;
