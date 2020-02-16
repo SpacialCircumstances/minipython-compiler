da3379185d27c0b9684d7d8ae35f0dac850ba7e7
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2020-02-16 15:17:22 +0100

    Run parser

--- minipython/src/parser.rs
+++ minipython/src/parser.rs
@@ -1,3 +1,10 @@
-use crate::ast;
+use crate::ast::*;
+use crate::name::*;
 
 lalrpop_mod!(pub minipython);
+
+fn parse(code: &str) -> Ast {
+    let mut name_store = NameStore::new();
+    let parser = minipython::ExprParser::new();
+    parser.parse(&mut name_store, code).unwrap()
+}
