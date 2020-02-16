d6d8030e59e9645c1428c888e04fe3e18762ce21
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2020-02-16 18:49:46 +0100

    Remove some unneeded code

--- minipython/src/lexer.rs
+++ minipython/src/lexer.rs
@@ -54,62 +54,6 @@ pub struct Location {
     pos: usize
 }
 
-
-struct Position<'input> {
-    chars: Chars<'input>,
-    pos: usize,
-    line: usize,
-    col: usize
-}
-
-impl<'input> Position<'input> {
-    fn from_chars(c: Chars<'input>) -> Position {
-        Position {
-            chars: c,
-            pos: 0,
-            line: 1,
-            col: 1
-        }
-    }
-
-    fn current(&self) -> Location {
-        Location {
-            pos: self.pos,
-            line: self.line,
-            col: self.col
-        }
-    }
-}
-
-impl<'input> Iterator for Position<'input> {
-    type Item = (Location, char);
-
-    fn next(&mut self) -> Option<Self::Item> {
-        match self.chars.next() {
-            None => None,
-            Some('\n') => {
-                let pos = self.current();
-                self.line += 1;
-                self.col = 1;
-                self.pos += 1;
-                Some((pos, '\n'))
-            },
-            Some(x) => {
-                let pos = self.current();
-                self.col += 1;
-                self.pos += 1;
-                Some((pos, x))
-            }
-        }
-    }
-}
-
-pub enum LexerState {
-    LineStart,
-    Comment,
-    Next
-}
-
 pub struct Lexer<'input> {
     chars: Peekable<Chars<'input>>,
     input: &'input str,
