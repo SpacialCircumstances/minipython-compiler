d0b484aeb98a9a26f69793e75bb7338e491f8cec
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2020-02-16 14:33:55 +0100

    Define name-based AST

--- minipython/src/ast.rs
+++ minipython/src/ast.rs
@@ -2,4 +2,10 @@ use crate::name::Name;
 
 #[derive(Debug, Eq, PartialEq)]
 pub enum Ast {
+    Def { name: Name, parameters: Vec<Name>, body: Vec<Ast> },
+    Return { name: Name },
+    While { cond_var: Name, body: Vec<Ast> },
+    Assign { var_name: Name, fun_name: Name, args: Vec<Name> },
+    Incr { var_name: Name },
+    Decr { var_name: Name }
 }
