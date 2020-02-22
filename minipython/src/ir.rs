8723ebc25697136c15c5395a1554d62dc6286b03
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2020-02-22 22:36:49 +0100

    Work on IR types

--- minipython/src/ir.rs
+++ minipython/src/ir.rs
@@ -1,1 +1,26 @@
+use crate::value::Value;
 
+pub struct IRFunction {
+    params: Vec<Value>,
+    body: Vec<IRStatement>
+}
+
+pub enum IRStatement {
+    ValueCreate(Value),
+    ValueModify(Value, i64),
+    FunctionCall {
+        func: IRFunction,
+        args: Vec<Value>,
+        target: Value
+    },
+    Loop {
+        condition_var: Value,
+        body: Vec<IRStatement>
+    },
+    Return(Value)
+}
+
+pub struct IRProgram {
+    functions: Vec<IRFunction>,
+    main: Vec<IRStatement>
+}
