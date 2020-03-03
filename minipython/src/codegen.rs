eb66ef55b8f2503ad23bc50c01ef844af57b90a5
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2020-03-03 23:59:01 +0100

    Write IO header

--- minipython/src/codegen.rs
+++ minipython/src/codegen.rs
@@ -1,9 +1,12 @@
 use crate::ir::*;
 use crate::name::*;
 use std::error::Error;
-use std::io::BufWriter;
+use std::io::{BufWriter, Write};
 use std::fs::File;
 
 pub fn compile_to_c(program: &IRProgram, name_store: &NameStore, output: &mut BufWriter<&File>) -> Result<(), Box<dyn Error>> {
+    //Include stdio
+    writeln!(output, "#include <stdio.h>");
+
     Ok(())
 }
